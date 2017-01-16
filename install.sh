572dfd05999d6104d0e1fe7f9d9432296258ed08
Paul Kinsler <dr.paul.kinsler@physics.org>
2017-01-16 12:29:34 +0000

    add install script

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Paul Kinsler 2016+
+#
+echo "$0: This script simply makes some links in the home directories"
+echo "    bin and lib to the actual location of the files - i.e. where 
+echo "    this script is run from."
+
+CORENAME=m2pvpk
+echo -n "$0/${CORENAME}:"
+
+IIC=0
+DIE=$(pwd)
+for DDD in lib meep mpb 
+do
+  cd ${DDD}
+  XDIR=$(pwd)
+  echo -n " $DDD"
+  if [ ${DDD} != "lib" ] ; then 
+    DDR=bin
+  else
+    DDR=lib
+  fi
+  for III in *
+  do
+    echo -n "."
+    IIC=$(( ${IIC} + 1 ))
+    ln -sf ${XDIR}/${III} ~/${DDR}/
+  done
+  cd ${DIE}
+done
+
+echo 
+echo $0/${CORENAME}: ${IIC} files copied
